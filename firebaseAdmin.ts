cbb79d3435890470f602c3f70e3d6aacc0911bcd
ryan-jones <ryanllewellynjones@gmail.com>
2023-03-21 16:11:40 +0100

    move away from secret.json to deploy to vercel

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,10 +1,21 @@
 import * as firebaseAdmin from 'firebase-admin';
 import { ServiceAccount } from 'firebase-admin';
-import serviceAccount from './serviceAccountKey.json';
 
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(serviceAccount as ServiceAccount),
+    credential: firebaseAdmin.credential.cert({
+      type: process.env.NEXT_PUBLIC_FIREBASE_TYPE,
+      project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      private_key_id: process.env.NEXT_PUBLIC_FIREBASE_PKEY_ID,
+      private_key: process.env.NEXT_PUBLIC_FIREBASE_PKEY,
+      client_email: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+      client_id: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_ID,
+      auth_uri: process.env.NEXT_PUBLIC_FIREBASE_AUTH_URI,
+      token_uri: process.env.NEXT_PUBLIC_FIREBASE_TOKEN_URI,
+      auth_provider_x509_cert_url:
+        process.env.NEXT_PUBLIC_FIREBASE_AUTH_CERT_URL,
+      client_x509_cert_url: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_CERT_URL,
+    } as ServiceAccount),
   });
 }
 
